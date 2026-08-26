c82eaa39444b87db542d71753a8335bca5088d13
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2026-08-26 16:59:50 -0300

    [neo-application] add/extract the memory sanity checker too
    Signed-off-by: Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>

--- crates/neo-application/src/lib.rs
+++ crates/neo-application/src/lib.rs
@@ -7,6 +7,7 @@
 mod columns;
 mod continuity;
 mod memory;
+mod memory_check;
 mod r1cs;
 mod relation;
 
@@ -14,6 +15,9 @@ pub use columns::{ColumnFamilySpec, ColumnRegistry, ColumnRegistryError, ColumnW
 pub use continuity::{ContinuityCatalog, ContinuityCatalogError, ContinuityGroup, ContinuityLink};
 pub use memory::{
     MemoryCatalog, MemoryCatalogError, MemoryKind, MemoryPortActivation, MemoryPortKind, MemoryPortSpec, MemorySpec,
+};
+pub use memory_check::{
+    check_memory_rows, MemoryCheckError, MemoryCheckPolicy, MemoryCheckPolicyError, MemoryPreload, RamInitialization,
 };
 pub use r1cs::{
     ConstraintCatalog, ConstraintTag, R1csBuildError, R1csBuilder, R1csRelation, R1csRow, R1csSide, TaggedR1csBuilder,
